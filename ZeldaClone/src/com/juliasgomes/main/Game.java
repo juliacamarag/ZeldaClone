d9eeb0e15f2ddc86a4d93e791d834c2ce8a4da25
juliasgomes <67568246+juliasgomes@users.noreply.github.com>
2022-02-21 01:03:43 -0300

    Tiles and validating positions

--- ZeldaClone/src/com/juliasgomes/main/Game.java
+++ ZeldaClone/src/com/juliasgomes/main/Game.java
@@ -42,10 +42,10 @@ public class Game extends Canvas implements Runnable, KeyListener {
 		setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
 		initFrame();
 		//Initiating objects
-		world = new World("/map.png");
 		image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
 		entities = new ArrayList<Entity>();
 		spritesheet = new Spritesheet("/spritesheet.png");
+		world = new World("/map.png");
 		player = new Player(0,0,16,16,spritesheet.getSprite(32, 0, 16, 16));
 		entities.add(player);
 	}
@@ -93,12 +93,12 @@ public class Game extends Canvas implements Runnable, KeyListener {
 			return;
 		}
 		Graphics g = image.getGraphics();
-		g.setColor(new Color(60,200,155));
+		g.setColor(new Color(0,0,0));
 		g.fillRect(0,0,WIDTH,HEIGHT);
 		
 		/*Game Rendering*/
 		//Graphics2D g2 = (Graphics2D) g;
-		
+		world.render(g);
 		for(int i = 0; i < entities.size(); i++) {
 			Entity e = entities.get(i);
 			e.render(g);
--- ZeldaClone/src/com/juliasgomes/world/World.java
+++ ZeldaClone/src/com/juliasgomes/world/World.java
@@ -1,24 +1,51 @@
 package com.juliasgomes.world;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 
 public class World {
+	
+	private Tile[] tiles;
+	public static int WIDTH, HEIGHT;
 	
 	public World(String path) {
 		try {
 			BufferedImage map = ImageIO.read(getClass().getResource(path));
 			int[] pixels = new int[map.getWidth() * map.getHeight()];
+			WIDTH = map.getWidth();
+			HEIGHT = map.getHeight();
+			tiles = new Tile[map.getWidth() * map.getHeight()];
 			map.getRGB(0,0,map.getWidth(),map.getHeight(), pixels, 0, map.getWidth());
-			for(int i = 0; i < pixels.length;i++) {
-				if(pixels[i] == 0xffff0000) {
-					System.out.println("Estou no pixel vermelho");
+			for(int xx = 0; xx < map.getWidth(); xx++) {
+				for(int yy = 0; yy < map.getHeight(); yy++) {
+					int curPixel = pixels[xx + (yy * map.getWidth())];
+					
+					if(curPixel == 0xFF000000) {
+						//Floor
+						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16,yy * 16, Tile.TILE_FLOOR);
+					}else if(curPixel == 0xFFFFFFFF) {
+						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_WALL);
+					}else if (curPixel == 0xFF0000ff) {
+						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+					}else {
+						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+					}
 				}
+				
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public void render(Graphics g) {
+		for(int xx = 0; xx < WIDTH; xx++) {
+			for(int yy = 0; yy < HEIGHT; yy++) {
+				Tile tile  = tiles[xx + (yy * WIDTH)];
+				tile.render(g);
+			}
 		}
 	}
 }
