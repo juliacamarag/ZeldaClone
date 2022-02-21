b210635928de2d74b1fba1859d6284da3601b905
juliasgomes <67568246+juliasgomes@users.noreply.github.com>
2022-02-21 19:21:56 -0300

    Entities in the map

--- ZeldaClone/src/com/juliasgomes/entities/Entity.java
+++ ZeldaClone/src/com/juliasgomes/entities/Entity.java
@@ -3,7 +3,14 @@ package com.juliasgomes.entities;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+import com.juliasgomes.main.Game;
+
 public class Entity {
+	
+	public static BufferedImage LIFEPACK_EN = Game.spritesheet.getSprite(6*16,0,16,16);
+	public static BufferedImage WEAPON_EN = Game.spritesheet.getSprite(7*16, 0, 16, 16);
+	public static BufferedImage BULLET_EN = Game.spritesheet.getSprite(6*16, 16, 16, 16);
+	public static BufferedImage ENEMY_EN = Game.spritesheet.getSprite(7*16, 16, 16, 16);
 
 	protected double x;
 	protected double y;
