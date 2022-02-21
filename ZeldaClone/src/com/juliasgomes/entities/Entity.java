4e8215d8541c23b2313b4216e2ae3b48e8d382cc
juliasgomes <67568246+juliasgomes@users.noreply.github.com>
2022-02-21 19:43:02 -0300

    Camera System

--- ZeldaClone/src/com/juliasgomes/entities/Entity.java
+++ ZeldaClone/src/com/juliasgomes/entities/Entity.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 import com.juliasgomes.main.Game;
+import com.juliasgomes.world.Camera;
 
 public class Entity {
 	
@@ -56,7 +57,7 @@ public class Entity {
 	}
 	
 	public void render(Graphics g) {
-		g.drawImage(sprite, this.getX(), this.getY(),null);
+		g.drawImage(sprite, this.getX() - Camera.x, this.getY() - Camera.y,null);
 	}
 	
 }
